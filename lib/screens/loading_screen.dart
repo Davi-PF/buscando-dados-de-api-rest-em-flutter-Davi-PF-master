85d184159eeab3b4b8e54bc66fd442433cbc4141
Davi-PF <daviprufer@gmail.com>
2022-10-25 18:53:54 -0300

    Initial commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../services/location.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  Location l = Location();
+
+  @override
+  void initState() {
+    super.initState();
+
+    l.getCurrentLocation();
+  }
+
+  void getData() async {
+    var url = Uri.parse('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) { // se a requisição foi feita com sucesso
+      var data = response.body;
+      print(data);  // imprima o resultado
+    } else {
+      print(response.statusCode);  // senão, imprima o código de erro
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    getData();
+
+    // TODO: implement build
+    return Scaffold();
+  }
+}
